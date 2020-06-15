ac0a963e76e67cc6282c587e4ae48dcf6ab32ff2
Monir Hossain <mknishad1@ymail.com>
2020-06-15 19:09:36 +0600

    desgin splash screen

--- app/src/main/java/com/shurjomukhi/shurjopay/SplashActivity.kt
+++ app/src/main/java/com/shurjomukhi/shurjopay/SplashActivity.kt
@@ -1,11 +1,14 @@
 package com.shurjomukhi.shurjopay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_splash)
+
+    startActivity(Intent(this, MainActivity::class.java))
+    finish()
   }
 }
