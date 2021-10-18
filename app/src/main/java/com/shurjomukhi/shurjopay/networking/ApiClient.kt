e8c3556fe96a2498d9b0b7eb427464faae423cf8
Monir <mknishad1@ymail.com>
2021-10-18 16:27:44 +0600

    add retrofit

---
+++ app/src/main/java/com/shurjomukhi/shurjopay/networking/ApiClient.kt
@@ -0,0 +1,74 @@
+package com.shurjomukhi.shurjopay.networking
+
+import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+
+class ApiClient {
+  private var retrofit: Retrofit? = null
+
+  fun getApiClient(baseUrl: String): Retrofit? {
+    if (retrofit == null) {
+      val interceptor = HttpLoggingInterceptor()
+      interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+      val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(50, TimeUnit.SECONDS)
+        .writeTimeout(50, TimeUnit.SECONDS)
+        .readTimeout(50, TimeUnit.SECONDS)
+        .addInterceptor(interceptor)
+        .build()
+
+      val gson = GsonBuilder().setLenient().create()
+
+      retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(okHttpClient)
+        .build()
+    }
+    return retrofit
+  }
+
+  fun getAuthApiClient(baseUrl: String, token: String): Retrofit? {
+    if (retrofit == null) {
+      val interceptor = HttpLoggingInterceptor()
+      interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+      val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(50, TimeUnit.SECONDS)
+        .writeTimeout(50, TimeUnit.SECONDS)
+        .readTimeout(50, TimeUnit.SECONDS)
+        .addInterceptor(interceptor)
+        .addInterceptor(object : Interceptor {
+          @Throws(IOException::class)
+          override fun intercept(chain: Interceptor.Chain): Response {
+            val newRequest: Request = chain.request().newBuilder()
+              .addHeader("Authorization", "Bearer $token")
+              .build()
+            return chain.proceed(newRequest)
+          }
+        }).build()
+
+      val gson = GsonBuilder().setLenient().create()
+
+      retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(okHttpClient)
+        .build()
+    }
+    return retrofit
+  }
+}
---
+++ app/src/main/java/com/shurjomukhi/shurjopay/networking/ApiInterface.kt
@@ -0,0 +1,57 @@
+package com.shurjomukhi.shurjopay.networking
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiInterface {
+
+  ///////////////////// GET ///////////////////
+
+  /*@GET("insertcheck/{mobile_number}")
+  fun insertCheck(
+    @Path("mobile_number") mobileNumber: String
+  ): Call<InsertCheck>
+
+  @GET("counterNumbers/createCounterNumber/")
+  fun createCounterNumber(
+    @Header("Authorization") token: String
+  ): Call<CounterNumber>
+
+  @GET("customers/getByMobileNumber/{mobile_number}")
+  fun getCustomer(
+    @Header("Authorization") token: String,
+    @Path("mobile_number") mobileNumber: String
+  ): Call<Customer>*/
+
+  //////////////////// POST ///////////////////
+
+  /*@POST("customers")
+  fun signUp(
+    @Body signUp: SignUpData
+  ): Call<SignUp>
+
+  @POST("verify_input_code")
+  fun verifyOtp(
+    @Body otpData: OtpData
+  ): Call<String>
+
+  @POST("update_customer_logins")
+  fun updateLogin(
+    @Body updateLogin: UpdateLogin
+  ): Call<UpdateLogin>
+
+  @POST("auth/login")
+  fun login(
+    @Body login: Login
+  ): Call<String>
+
+  @POST("agentRegistrationFromWeb")
+  fun registerAgent(
+    @Body agentRegistrationData: AgentRegistrationData
+  ): Call<AgentRegistration>
+
+  @POST("breedOfAnimals/typeOfBreeds")
+  fun getAnimalBreed(
+    @Body breed: Breed
+  ): Call<Breed>*/
+}
