9cfe8ee55141912e5add44b51fc036ad8851bdc9
Monir <mknishad1@ymail.com>
2021-10-28 16:13:19 +0600

    add and design login activity

--- app/src/main/java/com/shurjomukhi/shurjopay/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/shurjomukhi/shurjopay/ui/activity/SplashActivity.kt
@@ -8,7 +8,7 @@ class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    startActivity(Intent(this, MainActivity::class.java))
+    startActivity(Intent(this, LoginActivity::class.java))
     finish()
   }
 }
