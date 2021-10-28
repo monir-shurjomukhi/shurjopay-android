2ae0e8ca547e7db57dd85bfe8b75f568c3d4a076
Monir <mknishad1@ymail.com>
2021-10-28 16:32:07 +0600

    add and design signup activity

---
+++ app/src/main/java/com/shurjomukhi/shurjopay/ui/activity/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.shurjomukhi.shurjopay.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shurjomukhi.shurjopay.R
+
+class RegistrationActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_registration)
+  }
+}
