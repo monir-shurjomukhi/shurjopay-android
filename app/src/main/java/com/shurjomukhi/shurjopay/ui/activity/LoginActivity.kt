3992c6e22682a847d46af369fad92aed01613379
Monir <mknishad1@ymail.com>
2021-10-28 16:51:51 +0600

    set open and back action of registration activity

--- app/src/main/java/com/shurjomukhi/shurjopay/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/shurjomukhi/shurjopay/ui/activity/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.shurjomukhi.shurjopay.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shurjomukhi.shurjopay.R
+import com.shurjomukhi.shurjopay.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityLoginBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_login)
+    binding = ActivityLoginBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.registerTextView.setOnClickListener {
+      startActivity(Intent(this, RegistrationActivity::class.java))
+    }
   }
 }
--- app/src/main/java/com/shurjomukhi/shurjopay/ui/activity/RegistrationActivity.kt
+++ app/src/main/java/com/shurjomukhi/shurjopay/ui/activity/RegistrationActivity.kt
@@ -1,12 +1,38 @@
 package com.shurjomukhi.shurjopay.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.shurjomukhi.shurjopay.R
+import com.shurjomukhi.shurjopay.databinding.ActivityLoginBinding
+import com.shurjomukhi.shurjopay.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityRegistrationBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_registration)
+    binding = ActivityRegistrationBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    // add back arrow to toolbar
+    if (supportActionBar != null) {
+      supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+      supportActionBar!!.setDisplayShowHomeEnabled(true)
+    }
+
+    binding.loginTextView.setOnClickListener {
+      onBackPressed()
+    }
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // handle arrow click here
+    if (item.itemId == android.R.id.home) {
+      onBackPressed()
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
