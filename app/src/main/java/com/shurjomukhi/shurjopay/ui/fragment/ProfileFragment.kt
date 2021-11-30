28a5995c8fff03a051cd0c8f7312162d03120ec3
Monir <mknishad1@ymail.com>
2021-11-30 18:19:32 +0600

    perform logout

--- app/src/main/java/com/shurjomukhi/shurjopay/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/shurjomukhi/shurjopay/ui/fragment/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.shurjomukhi.shurjopay.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.shurjomukhi.shurjopay.databinding.FragmentProfileBinding
+import com.shurjomukhi.shurjopay.ui.activity.LoginActivity
 import com.shurjomukhi.shurjopay.ui.viewmodel.ProfileViewModel
 
 class ProfileFragment : Fragment() {
@@ -27,8 +29,25 @@ class ProfileFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    binding.logoutCardView.setOnClickListener {
+    /*viewModel.progress.observe(viewLifecycleOwner, {
+      if (it) {
+        showProgress()
+      } else {
+        hideProgress()
+      }
+    })
+    viewModel.message.observe(viewLifecycleOwner, {
+      shortSnack(binding.root, it)
+    })*/
+    viewModel.logout.observe(viewLifecycleOwner, {
+      if (it.message.equals("1")) {
+        startActivity(Intent(requireContext(), LoginActivity::class.java))
+        activity?.finishAffinity()
+      }
+    })
 
+    binding.logoutCardView.setOnClickListener {
+      viewModel.logout()
     }
   }
 }
--- app/src/main/java/com/shurjomukhi/shurjopay/ui/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/shurjomukhi/shurjopay/ui/viewmodel/ProfileViewModel.kt
@@ -1,13 +1,51 @@
 package com.shurjomukhi.shurjopay.ui.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.shurjomukhi.shurjopay.R
 import com.shurjomukhi.shurjopay.model.Logout
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 class ProfileViewModel(application: Application) : BaseViewModel(application) {
 
   private val _logout = MutableLiveData<Logout>()
   val logout: LiveData<Logout>
     get() = _logout
+
+  fun logout() {
+    viewModelScope.launch {
+      progress.value = true
+      val logout = Logout(preference.getMobileNumber().toString(), null)
+
+      val response = try {
+        apiClient.logout(logout)
+      } catch (e: IOException) {
+        Log.e(TAG, "logout: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      } catch (e: HttpException) {
+        Log.e(TAG, "logout: ${e.message}", e)
+        progress.value = false
+        message.value = R.string.unable_to_connect
+        return@launch
+      }
+
+      progress.value = false
+      if (response.isSuccessful && response.body() != null) {
+        _logout.value = response.body()
+      } else {
+        message.value = R.string.unable_to_connect
+      }
+    }
+  }
+
+  companion object {
+    private const val TAG = "ProfileViewModel"
+  }
 }
